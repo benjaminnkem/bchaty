9328ca60c0b592df091007b578d1baa66cce03e7
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-07-13 22:09:28 +0100

    Created Login Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,28 @@
+import Login from "@/components/Login";
 import SignUp from "@/components/SignUp";
-import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectAction, setSelectedAction] = useState<string>("none");
+
   return (
     <main>
-      <div className="flex items-center justify-center w-full min-h-screen">
+    <div className="flex items-center justify-center w-full min-h-screen">
         <div className="p-4 text-center border-2 border-gray-500 rounded-md shadow-md">
           <h1 className="text-2xl font-bold">BChaty</h1>
 
           <div className="flex justify-between mt-4 space-x-2 text-purple-50">
-            <Link href={"/chat"}>
-              <button className="px-4 py-1 bg-purple-700 rounded-md">Sign Up 🚀</button>
-            </Link>
-            <Link href={"/chat"}>
-              <button className="px-4 py-1 bg-purple-700 rounded-md">Login 🎯</button>
-            </Link>
+            <button className="px-4 py-1 bg-purple-700 rounded-md" onClick={() => setSelectedAction("signup")}>
+              Sign Up 🚀
+            </button>
+            <button className="px-4 py-1 bg-purple-700 rounded-md" onClick={() => setSelectedAction("login")}>
+              Login 🎯
+            </button>
           </div>
         </div>
 
-        <SignUp />
+        <SignUp selectAction={selectAction} setSelectAction={setSelectedAction} />
+        <Login selectAction={selectAction} setSelectAction={setSelectedAction} />
       </div>
     </main>
   );
