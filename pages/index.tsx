f758ad06f7651c9ddc3a259f015e78530ca82739
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-07-15 14:18:58 +0100

    Added useSession for home buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Login from "@/components/Login";
 import SignUp from "@/components/SignUp";
+import { signOut, useSession } from "next-auth/react";
 import Head from "next/head";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function Home() {
   const [selectAction, setSelectedAction] = useState<string>("none");
+  const { data: session, status } = useSession();
 
   return (
     <>
@@ -19,14 +22,29 @@ export default function Home() {
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi eligendi
             </p>
 
-            <div className="flex justify-center mt-4 space-x-2 text-purple-50">
-              <button className="px-4 py-1 bg-purple-700 rounded-md" onClick={() => setSelectedAction("signup")}>
-                Sign Up 🚀
-              </button>
-              <button className="px-4 py-1 bg-purple-700 rounded-md" onClick={() => setSelectedAction("login")}>
-                Login 🎯
-              </button>
-            </div>
+            {session ? (
+              <>
+                <div className="flex justify-center mt-4 space-x-2 text-purple-50">
+                  <Link href={"/chat"}>
+                    <button className="px-4 py-1 bg-purple-700 rounded-md">
+                      View Chats 〽
+                    </button>
+                  </Link>
+                  <button className="px-4 py-1 bg-purple-700 rounded-md" onClick={() => signOut()}>
+                    Log out
+                  </button>
+                </div>
+              </>
+            ) : (
+              <div className="flex justify-center mt-4 space-x-2 text-purple-50">
+                <button className="px-4 py-1 bg-purple-700 rounded-md" onClick={() => setSelectedAction("signup")}>
+                  Sign Up 🚀
+                </button>
+                <button className="px-4 py-1 bg-purple-700 rounded-md" onClick={() => setSelectedAction("login")}>
+                  Login 🎯
+                </button>
+              </div>
+            )}
           </div>
 
           <SignUp selectAction={selectAction} setSelectAction={setSelectedAction} />
